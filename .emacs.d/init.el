0cf01b6a57c52a36bef73d323b26775409696edd
Takuya Tanaka <banchou@peixe.biz>
2016-07-04 07:02:43 +0900

    普段使わないものを後にした

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -239,27 +239,6 @@
 
 
 
-;; blockdiag-mode
-
-(load-library "blockdiag-mode")
-(setq auto-mode-alist
-      (append '(("\\.diag$" . blockdiag-mode)) auto-mode-alist))
-
-
-
-;; sphinx
-
-;; Emacs起動時にrst.elを読み込み
-(use-package rst)
-;; 拡張子の*.rst, *.restのファイルをrst-modeで開く
-(setq auto-mode-alist
-      (append '(("\\.rst$" . rst-mode)
-				("\\.rest$" . rst-mode)) auto-mode-alist))
-;; 背景が黒い場合はこうしないと見出しが見づらい
-(setq frame-background-mode 'dark)
-;; 全部スペースでインデントしましょう
-(add-hook 'rst-mode-hook '(lambda() (setq indent-tabs-mode nil)))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; web-mode.el
@@ -317,3 +296,26 @@
 ;;   '(load-library "sql-indent"))
 ;; (sql-set-product "mysql")
 ;; (put 'downcase-region 'disabled nil)
+
+
+;; blockdiag-mode
+
+(load-library "blockdiag-mode")
+(setq auto-mode-alist
+      (append '(("\\.diag$" . blockdiag-mode)) auto-mode-alist))
+
+
+
+;; sphinx
+
+;; Emacs起動時にrst.elを読み込み
+(use-package rst)
+;; 拡張子の*.rst, *.restのファイルをrst-modeで開く
+(setq auto-mode-alist
+      (append '(("\\.rst$" . rst-mode)
+				("\\.rest$" . rst-mode)) auto-mode-alist))
+;; 背景が黒い場合はこうしないと見出しが見づらい
+(setq frame-background-mode 'dark)
+;; 全部スペースでインデントしましょう
+(add-hook 'rst-mode-hook '(lambda() (setq indent-tabs-mode nil)))
+
