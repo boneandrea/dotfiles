3ff26e16cc7c17db764ddaa4c7da46e85b6c8a0b
Your Name <you@example.com>
2018-09-09 13:28:18 +0900

    Add init-loader and use-package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,7 +5,24 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (package-initialize)
-(require 'use-package)
+
+;; init-loader, use-packageのインストール(caskで入らない)
+(require 'package)
+(add-to-list 'package-archives '("melpa"."http://melpa.org/packages/"))
+(package-initialize)
+(unless package-archive-contents (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+;; init-loader, use-packageのインストール(caskで入らない)
+(unless (package-installed-p 'init-loader)
+  (package-install 'init-loader))
+
+
+;; (unless (package-installed-p 'use-package)
+;;   (package-install 'use-package))
+
+
 
 (add-to-list 'load-path "~/.emacs.d/local-lisp")
 
