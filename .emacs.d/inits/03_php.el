c8205956dfedd21787f222f3382eaf4388a077c8
boneandrea <bone.andrea.anthrax@gmail.com>
2018-11-23 07:45:19 +0900

    Fix php-mode

--- .emacs.d/inits/03_php.el
+++ .emacs.d/inits/03_php.el
@@ -14,8 +14,6 @@
 (add-hook 'php-mode-hook
           '(lambda ()
 
-             (c-set-style "stroustrup")
-             (c-set-offset 'comment-intro 0)
              (setq tab-width 4)
              (setq c-basic-offset 4)
              (setq indent-tabs-mode nil)
