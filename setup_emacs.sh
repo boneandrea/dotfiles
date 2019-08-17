0efcc25299561a37a027df8d82300526d9bf55a1
Takuya Tanaka <banchou@peixe.biz>
2019-08-17 10:41:26 +0900

    Fix installer

--- setup_emacs.sh
+++ setup_emacs.sh
@@ -6,13 +6,10 @@ TARGET=~/.emacs.d
 
 mkdir -p $TARGET
 DIR=`pwd`
-FILES="Cask init.el inits local-lisp"
+FILES="init.el inits local-lisp"
 
 for f in `ls ./.emacs.d` ; do
   rm -rf ~/.emacs.d/$f
   ln -s $DIR/.emacs.d/$f ~/.emacs.d/$f
 done
-
-cd $TARGET
-cask install
 
