0659c434423b2ff17e2b2eb3dae1f1322f8ac639
Takuya Tanaka <banchou@peixe.biz>
2019-09-25 21:15:25 +0900

    Use .editorconfig and remove configures

--- .emacs.d/inits/04_web.el
+++ .emacs.d/inits/04_web.el
@@ -7,7 +7,7 @@
          ("\\.tpl\\.php$" . web-mode)
          ("\\.jsp$"       . web-mode)
          ("\\.as[cp]x$"   . web-mode)
-         ("\\.slim\\vue$"       . web-mode)
+         ("\\.slim$"      . web-mode)
          ("\\.erb$"       . web-mode)
          ("\\.html?$"     . web-mode))
   :config (
@@ -18,25 +18,22 @@
   :mode (("\\.pug$"     . pug-mode)))
 
 ;; ;;; 適用する拡張子
-;; (add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.ctp$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ctp$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
 
 ;;; インデント数
-(defun web-mode-hook ()
-  "Hooks for Web mode."
-  (setq web-mode-html-offset   2)
-  (setq web-mode-css-offset    2)
-  (setq web-mode-script-offset 2)
-  (setq web-mode-php-offset    2)
-  (setq web-mode-java-offset   2)
-  (setq web-mode-asp-offset    2)
-  (setq indent-tabs-mode t)
-  (setq web-mode-markup-indent-offset   2))
+;(defun web-mode-hook ()
+;  "Hooks for Web mode."
+;  (setq web-mode-html-offset   2)
+;  (setq web-mode-css-offset    2)
+;  (setq web-mode-script-offset 2)
+;  (setq web-mode-php-offset    2)
+;  (setq web-mode-java-offset   2)
+;  (setq web-mode-asp-offset    2)
+;  (setq indent-tabs-mode t)
+;  (setq web-mode-markup-indent-offset   2))
 
 (eval-after-load 'web-mode
   '(add-hook 'web-mode-hook #'add-node-modules-path))
