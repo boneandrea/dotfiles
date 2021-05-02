da15f69ce08f1c46c184068f17770263283e907c
boneandrea <bone.andrea.anthrax@gmail.com>
2021-05-02 12:55:34 +0900

    Fix sql-mode

--- .emacs.d/inits/07_sql.el
+++ .emacs.d/inits/07_sql.el
@@ -4,4 +4,17 @@
 
 (use-package sql-indent)
 
+;; SQL モードの雑多な設定
+(add-hook 'sql-mode-hook
+    (function (lambda ()
+                (setq sql-indent-offset 4)
+                (setq sql-indent-maybe-tab t)
+                (local-set-key "\C-cu" 'sql-to-update) ; sql-transform
+                 ;; SQLi の自動ポップアップ
+                   (setq sql-pop-to-buffer-after-send-region t)
+                ;; master モードを有効にし、SQLi をスレーブバッファにする
+                   (master-mode t)
+                (master-set-slave sql-buffer)
+                )))
+
 ;;; ends here
